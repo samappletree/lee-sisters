9683005c93c96b058535d0ff4b15a61071e89862
Samantha Applebaum <samappletree@gmail.com>
2019-08-04 10:39:32 -0700

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD9j8DQ3YwnAPwwa9iKjQbe6astnfOhUgg",
+    authDomain: "lee-sisters.firebaseapp.com",
+    databaseURL: "https://lee-sisters.firebaseio.com",
+    projectId: "lee-sisters",
+    storageBucket: "lee-sisters.appspot.com",
+    messagingSenderId: "152578187911",
+    appId: "1:152578187911:web:b844cf5aa615951b"
+  }
 };
